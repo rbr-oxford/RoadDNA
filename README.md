46e04ebcfd6cd06374bc0d8ade8a603fa8407c62
Drishya <adhikaridrishya5@gmail.com>
2026-08-01 18:00:08 +0545

    Remove all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# RoadDNA
-Smart road intelligence platform that detects, analyzes, and visualizes road conditions in real time.
--- src/data/nepalRoads.js
+++
@@ -1,5 +0,0 @@
-// src/data/nepalRoads.js
-// No fake road data. Will be populated with real GIS data later.
-export const nepalRoads = [];
-
-export default nepalRoads;
